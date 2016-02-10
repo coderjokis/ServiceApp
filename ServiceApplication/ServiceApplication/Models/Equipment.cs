ecba825c1ec31a5255326a0f7e580a594d27e063
Joseph Maglalang <joseph.maglalang@robertsoncollege.net>
2016-02-10 09:58:36 -0600

    Created classes

---
+++ ServiceApplication/ServiceApplication/Models/Equipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceApplication.Models
+{
+    public class Equipment
+    {
+        public int EquipmentID { get; set; }
+        public string EquipmentName { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public  string InstallDate { get; set; }
+        public string AuthorizingParty { get; set; }
+        public Equipment(){}
+        public Equipment(int equipmentID, string equipmentName, string description, string location, string installDate, string authorizingParty)
+        {
+            this.EquipmentID = equipmentID;
+            this.EquipmentName = equipmentName;
+            this.Description = description;
+            this.Location = location;
+            this.InstallDate = installDate;
+            this.AuthorizingParty = authorizingParty;      
+        }
+    }
+}
---
+++ ServiceApplication/ServiceApplication/Models/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceApplication.Models
+{
+    public class Inventory
+    {
+        public int InventoryID { get; set; }
+        public Item Item { get; set; }
+        public List<Item> InventoryItem { get; set; }
+    }
+}
